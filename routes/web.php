2c92b2a8bd98657bd600a0ae4810f822fcc65fb4
Wesley Kenji Ito Hidehira <kenjihidehira999@gmail.com>
2026-09-16 19:27:34 -0300

    Cria rota /products usando ProductController

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductItensController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Lista todos os produtos juntamente com seus itens
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+// Lista todos os itens de produto (JSON)
+Route::get('/product-itens', [ProductItensController::class, 'index'])->name('product-itens.index');
